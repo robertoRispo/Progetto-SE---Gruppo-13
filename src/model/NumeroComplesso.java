2ec90293d3a4ff3cd721f7e47754936c22fcefd5
Roberto <robris96@gmail.com>
2021-11-22 11:02:51 +0100

    classe implementata

--- src/model/NumeroComplesso.java
+++ src/model/NumeroComplesso.java
@@ -10,4 +10,47 @@ package model;
  */
 public class NumeroComplesso {
     
+    private double parteReale;
+    private double parteImmaginaria;
+    
+    public NumeroComplesso(double parteReale,double parteImmaginaria){
+        this.parteReale = parteReale;
+        this.parteImmaginaria = parteImmaginaria;
+    }
+    
+    public static NumeroComplesso somma (NumeroComplesso x, NumeroComplesso y) {
+        return new NumeroComplesso(x.parteReale + y.parteReale, x.parteImmaginaria + y.parteImmaginaria);
+    }
+    
+    public static NumeroComplesso sottrazione (NumeroComplesso x, NumeroComplesso y) {
+        return new NumeroComplesso(x.parteReale - y.parteReale, x.parteImmaginaria - y.parteImmaginaria);
+    }
+
+    public static NumeroComplesso moltiplicazione (NumeroComplesso x, NumeroComplesso y) {
+        double parteReale = x.parteReale*y.parteReale - x.parteImmaginaria*y.parteImmaginaria;
+        double parteImmaginaria = x.parteImmaginaria*y.parteReale + x.parteReale*y.parteImmaginaria;
+        return new NumeroComplesso(parteReale, parteImmaginaria);
+    }
+    public static NumeroComplesso moltiplicazione (double a, NumeroComplesso x) {
+        return new NumeroComplesso(a*x.parteReale, a*x.parteImmaginaria);
+    }
+    public static NumeroComplesso divisione (NumeroComplesso x, NumeroComplesso y) {
+        return moltiplicazione(x, moltiplicazione(1/y.radicequadrata()/y.radicequadrata(), y.coniugato()));
+    }
+    public NumeroComplesso coniugato(){
+        return new NumeroComplesso(parteReale, -parteImmaginaria);
+    }
+
+    public double reale(){
+        return parteReale;
+    }
+    public double immmaginaria(){
+        return parteImmaginaria;
+    }
+    public double radicequadrata(){
+        return Math.sqrt(parteReale*parteReale+parteImmaginaria*parteImmaginaria);
+    }
+    public double arg(){
+        return (2*Math.PI + Math.atan2(parteImmaginaria, parteReale)) % (2*Math.PI);
+    }
 }
