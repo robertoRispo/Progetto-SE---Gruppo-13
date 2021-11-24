dadcedc4dd292673b8ad4e0b3842387ccc50dca6
hjsplint <agostino.moffa@gmail.com>
2021-11-24 10:29:31 +0100

    Update Core.java
    Aggiunta javadoc a Core

--- src/model/Core.java
+++ src/model/Core.java
@@ -7,8 +7,9 @@ package model;
 import java.util.*;
 
 /**
- *
- * @author agostinomoffa
+ * La classe Core viene utilizzata per effettuare operazioni di modifica sullo stack
+ * Implementa l'interfaccia operazione che definisce le operazioni elementari che devono 
+ * essere implementate 
  */
 public class Core implements Operations {
 
@@ -18,22 +19,43 @@ public class Core implements Operations {
         data = StackSingle.getInstance();
     }
 
+    /**
+     * Il metodo viene prende in input due valori di tipo double, al primo
+     * associa parte real mentre al secondo associa la parte complessa, crea un 
+     * oggetto di tipo Numero Complesso e ne effettua il push nello stack
+     * @param a parte reale
+     * @param b parte immaginaria
+     */
     @Override
     public void creaNumero(double a, double b) {
         NumeroComplesso j = new NumeroComplesso(a, b);
         this.pushInStack(j);
     }
 
+    /**
+     * Metodo che effettua il push nello stack di un numero complesso
+     * @param a Numero Complesso
+     */
     @Override
     public void pushInStack(NumeroComplesso a) {
         data.StackPush(a);
     }
 
+    /**
+     * Metodo che effettua il pop dallo stack e ritorna il numero complesso che
+     * si trova nella cima dello stack
+     * @return Numero Complesso
+     */
     @Override
     public NumeroComplesso popFromStack() {
         return (NumeroComplesso) data.StackPop();
     }
 
+    /**
+     * Metodo per effettuare la somma tra due numeri complessi presenti nello stack
+     * Tale metodo prima effettua il Pop dei due numeri, ne fa la somma ed effettua il push
+     * del risultato nello stack
+     */
     @Override
     public void sommaInStack() {
         NumeroComplesso a, b, s;
@@ -53,6 +75,11 @@ public class Core implements Operations {
         }
     }
 
+    /**
+     * Metodo per effettuare la differenza tra due numeri complessi presenti nello stack
+     * Tale metodo prima effettua il Pop dei due numeri, ne fa la differenza ed effettua il push
+     * del risultato nello stack
+     */
     @Override
     public void diffInStack() {
         NumeroComplesso a, b, s;
@@ -72,6 +99,11 @@ public class Core implements Operations {
         }
     }
 
+    /**
+     * Metodo per effettuare il prodotto tra due numeri complessi presenti nello stack
+     * Tale metodo prima effettua il Pop dei due numeri, ne fa il prodotto ed effettua il push
+     * del risultato nello stack
+     */
     @Override
     public void prodInStack() {
         NumeroComplesso a, b, s;
@@ -91,6 +123,11 @@ public class Core implements Operations {
         }
     }
 
+    /**
+     * Metodo per effettuare la divisione tra due numeri complessi presenti nello stack
+     * Tale metodo prima effettua il Pop dei due numeri, ne fa la divisione ed effettua il push
+     * del risultato nello stack
+     */
     @Override
     public void divInStack() {
         NumeroComplesso a, b, s;
@@ -110,8 +147,11 @@ public class Core implements Operations {
         }
     }
 
-    
-    
+    /**
+     * Metodo per effettuare la radice quadra di un numero complesso presente nello stack
+     * Tale metodo prima effettua il Pop del numero, ne calcola la radice ed effettua il push
+     * del risultato nello stack
+     */
     @Override
     public void radiceInStack(){
         NumeroComplesso a, s;
@@ -123,8 +163,9 @@ public class Core implements Operations {
     
     }
     
-    
-    
+    /**
+     * Metodo utilizzato per stampare a schermo il contenuto dello stack.
+     */
     public void printStack() {
         System.out.println(data.stackPrint());
     }
